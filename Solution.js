27f1cde53cffe14b9b83ae60bf8e9eba7b8fe816
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-03 20:36:29 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,27 @@
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+var longestMonotonicSubarray = function (input) {
+
+    let startIndexIncreasing = 0;
+    let startIndexDecreasing = 0;
+    let maxSizeMonotonicSubarray = 0;
+
+    for (let i = 1; i < input.length; ++i) {
+        if (input[i] <= input[i - 1]) {
+            maxSizeMonotonicSubarray = Math.max(maxSizeMonotonicSubarray, i - startIndexIncreasing);
+            startIndexIncreasing = i;
+        }
+        if (input[i] >= input[i - 1]) {
+            maxSizeMonotonicSubarray = Math.max(maxSizeMonotonicSubarray, i - startIndexDecreasing);
+            startIndexDecreasing = i;
+        }
+    }
+
+    maxSizeMonotonicSubarray = Math.max(maxSizeMonotonicSubarray, input.length - startIndexIncreasing);
+    maxSizeMonotonicSubarray = Math.max(maxSizeMonotonicSubarray, input.length - startIndexDecreasing);
+
+    return maxSizeMonotonicSubarray;
+};
